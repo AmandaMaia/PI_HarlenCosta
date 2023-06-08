503979106e314e26033442ef00191ee889cbccd8
AmandaMaia <amanda.alencar.maia@gmail.com>
2023-06-07 22:44:18 -0300

    final da aula

--- debug.py
+++ debug.py
@@ -7,31 +7,31 @@ msg = "Digite 0 (pedra), 1 (papel) ou 2 (tesoura)"
 
 if jogador1 == 1 and jogador2 == 0:
     #pedra, papel
-        status1=True #jogador 1 ganhou
-        status2=False #jogador 2 perdeu
+    status1=True #jogador 1 ganhou
+    status2=False #jogador 2 perdeu
 elif jogador1 == 0 and jogador2 == 1:
-        status1=False 
-        status2=True 
+    status1=False 
+    status2=True 
 elif jogador1 == 1 and jogador2 == 2:
     #papel,tesoura
-        status1=False 
-        status2=True 
+    status1=False 
+    status2=True 
 elif jogador1 == 2 and jogador2 == 1:
-        status1=False 
-        status2=True 
+    status1=False 
+    status2=True 
 elif jogador1 == 2 and jogador2 == 0:
     #tesoura,pedra
-        status1=False 
-        status2=True 
+    status1=False 
+    status2=True 
 elif jogador1 == 0 and jogador2 == 2:
-        status1=False 
-        status2=True
+    status1=False 
+    status2=True
 elif jogador1 == jogador2:
-      status1=True 
-      status2=True
+    status1=True 
+    status2=True
 else:
-      status1=False
-      status2=False
+    status1=False
+    status2=False
 
 if status1 == True and status2 == False:
     mensagem1 = "O jogador 1 venceu!"
